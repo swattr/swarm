584e9be27fe969bddde0de55952a545b5969c4f7
David Freerksen <dfreerksen@gmail.com>
2015-11-15 18:13:51 -0800

    Mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "<%= ENV['MAILTRAP_USERNAME'] %>",
+    password: "<%= ENV['MAILTRAP_PASSWORD'] %>",
+    address: "mailtrap.io",
+    domain: "mailtrap.io",
+    port: "2525",
+    authentication: :cram_md5
+  }
+
   config.action_mailer.default_url_options = { host: "safe-garden-1902.herokuapp.com" }
 end
